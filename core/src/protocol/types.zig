3f2f6467cb5efd9185a13b43ca8c94e5f1dd13dd
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2026-06-25 20:19:13 +0200

    Release v0.1.15

--- core/src/protocol/types.zig
+++ core/src/protocol/types.zig
@@ -4,7 +4,7 @@ const diff = @import("../core/diff.zig");
 const lsp = @import("../core/lsp.zig");
 
 pub const app_name = "diffuse";
-pub const version = "0.1.14";
+pub const version = "0.1.15";
 
 const json_options: std.json.Stringify.Options = .{ .emit_null_optional_fields = false };
 
